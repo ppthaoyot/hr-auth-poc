51f42e576c9fa445b77c8aae3a000d75a43f090a
Phongphan Thaoyot <57580564+ppthaoyot@users.noreply.github.com>
2025-09-03 13:11:33 +0700

    Add weather forecast service and controller

---
+++ HRAuthorization.Api/Services/WeatherService.cs
@@ -0,0 +1,22 @@
+
+namespace HRAuthorization.Api.Services;
+
+public class WeatherService : IWeatherService
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+        "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public IEnumerable<WeatherForecast> GetForecast()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
